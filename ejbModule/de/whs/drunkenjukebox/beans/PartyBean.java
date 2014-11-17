2bf8ec583b0dd16104ab128787c8fed764717946
chris.schluetter@gmail.com <chris.schluetter@gmail.com@73d7e5b3-49d1-5e8c-c12a-6e81650299de>
2014-11-17 11:01:00 +0000

    Projekt auf JDK 1.8.0_25 umgestellt und PartyBean hinzugefügt.

---
+++ ejbModule/de/whs/drunkenjukebox/beans/PartyBean.java
@@ -0,0 +1,20 @@
+package de.whs.drunkenjukebox.beans;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class PartyBean
+ */
+@Stateless
+@LocalBean
+public class PartyBean {
+
+    /**
+     * Default constructor. 
+     */
+    public PartyBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
