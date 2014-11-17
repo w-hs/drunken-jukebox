203674305934669c914506777eb6a41cd5649eed
chris.schluetter@gmail.com <chris.schluetter@gmail.com@73d7e5b3-49d1-5e8c-c12a-6e81650299de>
2014-11-17 14:08:51 +0000

    AdminBean: Song-Methoden implementiert

--- ejbModule/de/whs/drunkenjukebox/beans/admin/AdminBean.java
+++ ejbModule/de/whs/drunkenjukebox/beans/admin/AdminBean.java
@@ -1,10 +1,18 @@
 package de.whs.drunkenjukebox.beans.admin;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import de.whs.drunkenjukebox.model.Party;
+import de.whs.drunkenjukebox.model.Playlist;
 import de.whs.drunkenjukebox.model.Song;
 
 /**
@@ -13,47 +21,88 @@ import de.whs.drunkenjukebox.model.Song;
 @Stateless
 public class AdminBean implements IAdminRemote {
 
+	@PersistenceContext
+	private EntityManager em;
+	
     /**
      * Default constructor. 
      */
     public AdminBean() {
         // TODO Auto-generated constructor stub
     }
+    
+    private Party getCurrentParty() {
+    	Query q = em.createQuery("from Party p where p.endTime is null");
+    	List<?> resultList = q.getResultList();
+    	
+    	int size = resultList.size();    	
+    	if (size == 0)
+    		return null;    	
+    	if (size == 1)
+    		return (Party)resultList.get(0);
+    	
+    	throw new RuntimeException("Internal Server Error. More than one party!");
+    }
 
 	@Override
 	public Party startParty() {
+		if (getCurrentParty() != null)
+			throw new RuntimeException("Party already started!");
+		
+		Party p = new Party();
+		p.setStartTime(Calendar.getInstance());
+			
+		Playlist playlist = new Playlist();
+		fillPlaylist(playlist);
+		p.setPlaylist(playlist);
+		
+		Song song = chooseCurrentSong(playlist);
+		p.setCurrentSong(song);
+		
+		em.persist(p);
+		return p;
+	}
+	
+	private Song chooseCurrentSong(Playlist playlist) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private void fillPlaylist(Playlist playlist) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void stopParty() {
-		// TODO Auto-generated method stub
-		
+		Party current = getCurrentParty();
+		current.setEndTime(Calendar.getInstance());
 	}
 
 	@Override
 	public void addSongs(List<Song> songs) {
-		// TODO Auto-generated method stub
-		
+		for (Song song : songs) {
+			em.persist(song);
+		}
 	}
 
 	@Override
 	public List<Song> getSongs() {
-		// TODO Auto-generated method stub
-		return null;
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Song> cquery = builder.createQuery(Song.class);
+		TypedQuery<Song> query = em.createQuery(cquery);
+		return query.getResultList();
 	}
 
 	@Override
 	public void updateSong(Song s) {
-		// TODO Auto-generated method stub
-		
+		em.merge(s);
 	}
 
 	@Override
 	public void deleteSong(Song s) {
-		// TODO Auto-generated method stub
-		
+		Song mergedSong = em.merge(s);
+		em.remove(mergedSong);
 	}
 
 }
