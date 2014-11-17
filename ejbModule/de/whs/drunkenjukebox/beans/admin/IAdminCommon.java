a90c3b6830eb586c3cbcd887c5aa7a6c877d0566
chris.schluetter@gmail.com <chris.schluetter@gmail.com@73d7e5b3-49d1-5e8c-c12a-6e81650299de>
2014-11-17 12:02:23 +0000

    Methoden in den Interfaces erstellt...

--- ejbModule/de/whs/drunkenjukebox/beans/admin/IAdminCommon.java
+++ ejbModule/de/whs/drunkenjukebox/beans/admin/IAdminCommon.java
@@ -1,5 +1,17 @@
 package de.whs.drunkenjukebox.beans.admin;
 
-public interface IAdminCommon {
+import java.util.List;
 
+import de.whs.drunkenjukebox.model.Party;
+import de.whs.drunkenjukebox.model.Song;
+
+public interface IAdminCommon {
+	Party startParty();
+	void stopParty();
+
+	void addSongs(List<Song> songs);
+	List<Song> getSongs();
+	void updateSong(Song s);
+	void deleteSong(Song s);
+	
 }
--- ejbModule/de/whs/drunkenjukebox/beans/party/IPartyCommon.java
+++ ejbModule/de/whs/drunkenjukebox/beans/party/IPartyCommon.java
@@ -1,5 +1,25 @@
 package de.whs.drunkenjukebox.beans.party;
 
-public interface IPartyCommon {
+import java.util.List;
 
+import de.whs.drunkenjukebox.model.DIValue;
+import de.whs.drunkenjukebox.model.PartyPeople;
+import de.whs.drunkenjukebox.model.Playlist;
+import de.whs.drunkenjukebox.model.Song;
+import de.whs.drunkenjukebox.model.Vote;
+
+public interface IPartyCommon {
+	Playlist getPlaylist();
+	
+	Song getCurrentSong();
+	
+	PartyPeople registerPartyPeople();
+	
+	List<Vote> getVotes();
+	
+	List<DIValue> getDiValues();
+	
+	void vote(int partyPeopleId, int songId);
+	
+	void sendDi(int partyPeopleId, int di);
 }
