ef9879f8dc4f418045639fa8ab8c35bd4fcf5f2f
danielhardes@gmail.com <danielhardes@gmail.com@73d7e5b3-49d1-5e8c-c12a-6e81650299de>
2014-12-08 09:13:13 +0000

    Player Bean Authentication added

--- trunk/rest/src/main/java/de/whs/drunkenjukebox/beans/player/PlayerBean.java
+++ trunk/rest/src/main/java/de/whs/drunkenjukebox/beans/player/PlayerBean.java
@@ -1,12 +1,17 @@
 package de.whs.drunkenjukebox.beans.player;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateful;
 
 import de.whs.drunkenjukebox.model.SongSource;
+import de.whs.drunkenjukebox.rest.roles.IRoles;
 
 /**
  * Session Bean implementation class PartyBean
  */
+@DeclareRoles({IRoles.Player})	
+@RolesAllowed({IRoles.Player})
 @Stateful
 public class PlayerBean implements IPlayerRemote {
 
