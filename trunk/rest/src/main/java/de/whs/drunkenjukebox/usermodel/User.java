88b1305cc173c01cba54cae3e790e06dc8c93d96
chris.schluetter@gmail.com <chris.schluetter@gmail.com@73d7e5b3-49d1-5e8c-c12a-6e81650299de>
2014-12-08 10:44:13 +0000

    Role und User Entities hinzugefügt.

---
+++ trunk/rest/src/main/java/de/whs/drunkenjukebox/usermodel/User.java
@@ -0,0 +1,61 @@
+package de.whs.drunkenjukebox.usermodel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(nullable=false)
+	private String name;
+	
+	@Column(nullable=false)
+	private String password;
+	
+	@OneToMany(cascade={CascadeType.PERSIST})
+	private Collection<Role> roles = new ArrayList<Role>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+}
