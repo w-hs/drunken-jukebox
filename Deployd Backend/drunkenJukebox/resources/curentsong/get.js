ce5b90b844a98c08314d432b52e9298d99d5b0ca
Daniel <danielhardes@gmail.com>
2015-04-22 09:45:02 +0200

    Current song gibt ersten song der playlist zurück

--- Deployd Backend/drunkenJukebox/resources/curentsong/get.js
+++ Deployd Backend/drunkenJukebox/resources/curentsong/get.js
@@ -1,3 +1,23 @@
 //Playlist ersten entry entnehmen
 //Playlist wieder auffüllen
 //
+
+var votes = -999999;
+var songID = null;
+dpd.playlist.get(function (result, err) {
+  if(err) return console.log(err);
+  //sortieren und ersten Entry zurück geben
+ 
+  
+  for(var i = 0; i< result.length; i++)
+  {
+       
+    if(result[i].votes >= votes)
+     votes  = result[i].votes;
+     songID = result[i].songID;
+  }
+  
+  this.songID = songID;
+});
+
+
