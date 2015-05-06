38ba3c539a285a46e3c5f67511643e9ad8524ff4
fabian-paus <paus.fabian@gmail.com>
2015-05-06 09:23:21 +0200

    Frontend => VoteApp

--- Frontend/src/de/whs/drunkenjukebox/client/VoteApp.java
+++ Frontend/src/de/whs/drunkenjukebox/client/VoteApp.java
@@ -20,7 +20,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class Frontend implements EntryPoint {
+public class VoteApp implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
