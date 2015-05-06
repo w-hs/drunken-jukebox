2ee23d09ae1023ae93b4a0217afe2abd6f7927c0
fabian-paus <paus.fabian@gmail.com>
2015-05-06 13:17:39 +0200

    Mehr Warnungen entfernt

--- Frontend/src/de/whs/drunkenjukebox/client/voteapp/DISlider.java
+++ Frontend/src/de/whs/drunkenjukebox/client/voteapp/DISlider.java
@@ -2,7 +2,6 @@ package de.whs.drunkenjukebox.client.voteapp;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.ClientBundle.Source;
 import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.Image;
@@ -15,7 +14,7 @@ public class DISlider extends SliderBarHorizontal {
        public DISlider(int maxValue, String width) {
            //setLessWidget(new Image(images.less()));
            //setMoreWidget(new Image(images.more()));
-           setScaleWidget(new Image(images.scale().getUrl()), 5);
+           setScaleWidget(new Image(images.scale().getSafeUri()), 5);
           // setMoreWidget(new Image(images.more()));
            setDragWidget(new Image(images.drag()));
            this.setWidth(width);
