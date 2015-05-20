26749192eb0e6162a56d11d6adb6367e33218c0f
Daniel <danielhardes@gmail.com>
2015-05-20 14:25:42 +0200

    deployd anfrage über server

--- Frontend/src/de/whs/drunkenjukebox/server/VoteAppServiceImpl.java
+++ Frontend/src/de/whs/drunkenjukebox/server/VoteAppServiceImpl.java
@@ -1,8 +1,19 @@
 package de.whs.drunkenjukebox.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Console;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.whs.drunkenjukebox.shared.PlayList;
@@ -12,32 +23,89 @@ import de.whs.drunkenjukebox.shared.Vote;
 import de.whs.drunkenjukebox.shared.VoteAppService;
 import de.whs.drunkenjukebox.shared.VoteResult;
 
-public class VoteAppServiceImpl extends RemoteServiceServlet implements VoteAppService {
+public class VoteAppServiceImpl extends RemoteServiceServlet implements
+		VoteAppService {
 
 	private static final long serialVersionUID = 88682179774253373L;
-
+	private static final String ServerURL = "http://localhost:2403/";
+	
 	@Override
 	public Song getCurrentSong() {
 		// TODO: Wirklich implementieren
-		
-		List<String> genres = new ArrayList<String>();
-		genres.add("Classic");
-		genres.add("Pop");
-		
 		Song song = new Song();
-		song.setInterpret("Wham");
-		song.setTitle("Last Christmas");
-		song.setGenres(genres);
-		song.setSongSource("https://www.youtube.com/");
-		song.setDurationInSecs(186);
-	
+		
+		JSONArray currentSongArray = getJsonArray(ServerURL+"curentsong");
+		if(currentSongArray != null)
+		{
+			
+			try {
+				String songID = currentSongArray.getJSONObject(0).getString("songID");
+				JSONObject currentSong = getJsonObject(ServerURL+"songs/"+songID);
+				
+				
+				song.setInterpret(currentSong.getString("artist"));
+				song.setTitle(currentSong.getString("title"));
+				
+				
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		return song;
+	}
+
+	private JSONObject getJsonObject(String url) {
+		try {
+			@SuppressWarnings({ "deprecation", "resource" })
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(url);
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			String line = "";
+			String JsonString = "";
+			while ((line = rd.readLine()) != null) {
+				//System.out.println(line);
+				JsonString+=line;
+			}
+			
+			return new JSONObject(JsonString);
+			
+			
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	private JSONArray getJsonArray(String url) {
+		try {
+			@SuppressWarnings({ "deprecation", "resource" })
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(url);
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			String line = "";
+			String JsonString = "";
+			while ((line = rd.readLine()) != null) {
+				//System.out.println(line);
+				JsonString+=line;
+			}
+			
+			return new JSONArray(JsonString);
+			
+			
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
 	public PlayList getPlayList() {
 		// TODO: Wirklich implementieren
-		
+
 		PlayList pl = new PlayList();
 		PlayListEntry entry = new PlayListEntry();
 		entry.setSongName("Last Christmas");
