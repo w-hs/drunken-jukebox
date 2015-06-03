64fa0418a4f6e4fe15aa8360c33c64daa48ca424
Daniel <danielhardes@gmail.com>
2015-06-03 10:15:13 +0200

    Änderungen am server

--- Deployd Backend/drunkenJukebox/resources/party/post.js
+++ Deployd Backend/drunkenJukebox/resources/party/post.js
@@ -1,1 +1,39 @@
 //playlist füllen
+
+    this.start = new Date();
+    this.end = {};
+    this.avgDI = 0;
+    this.guestCount = 1;
+
+
+dpd.playlist.get(function (result, err) {
+    
+
+    for(var i = 0; i< result.length; i++)
+      {
+           
+           dpd.playlist.del(result[i].id,function(result, error) {});
+      }
+});
+
+dpd.songs.get(function (result, err) {
+  if(err) return console.log(err);
+
+    
+
+    for(var i = 0; i<3;i++)
+    {
+     
+      
+        dpd.playlist.post({songID: result[i].id ,position:0,votes:0}, function(result, error) {
+     
+         
+         });
+        
+    }
+
+
+  
+});
+
+
