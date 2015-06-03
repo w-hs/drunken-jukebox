bbe221066ddad9b682ba43584f443739b77343fa
fabian-paus <paus.fabian@gmail.com>
2015-06-03 11:51:42 +0200

    Lokalisierung: VoteApp

--- Frontend/src/de/whs/drunkenjukebox/resources/AppConstants.java
+++ Frontend/src/de/whs/drunkenjukebox/resources/AppConstants.java
@@ -47,4 +47,31 @@ public interface AppConstants extends Constants {
 	
 	@DefaultStringValue("Search...")
 	String searchDotDotDot();
+	
+	@DefaultStringValue("Send")
+	String send();
+	
+	@DefaultStringValue("Cancel")
+	String cancel();
+	
+	@DefaultStringValue("Please slide in your DI")
+	String pleaseSlideInDI();
+	
+	@DefaultStringValue("Sober")
+	String sober();
+	
+	@DefaultStringValue("Funny")
+	String funny();
+	
+	@DefaultStringValue("Drunk")
+	String drunk();
+	
+	@DefaultStringValue("Send DI")
+	String sendDI();
+	
+	@DefaultStringValue("Up")
+	String upVote();
+	
+	@DefaultStringValue("Down")
+	String downVote();
 }
